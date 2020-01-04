65e4cddef9b85695518d3b03a8cd48dbaa307607
shaodahong <darryshaw@gmail.com>
2020-01-04 13:40:17 +0800

    mention add upgradeMessage

--- src/_util/upgradeMessage.ts
+++ src/_util/upgradeMessage.ts
@@ -4,5 +4,7 @@ export default (component: string) =>
   warning(
     false,
     component,
-    `The legacy component has been deprecated, and ant design 4.0 now released! Please follow https://ant.design/components/${component.toLowerCase()} to upgrade.`,
+    `The legacy component has been deprecated, and ant design 4.0 now released! Please follow https://ant.design/components/${component.toLowerCase()}${
+      component === 'Mention' ? 's' : ''
+    } to upgrade.`,
   );
