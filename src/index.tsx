c4ae989f8ebb92049876e09cba159d879fde4dd7
二货机器人 <smith3816@gmail.com>
2022-10-12 17:12:10 +0800

    chore: clean up

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-export * from 'antd';
-export { default as AutoComplete } from './AutoComplete';
-export { default as Cascader } from './Cascader';
-export { default as Mentions } from './Mentions';
-export { default as Select } from './Select';
-export { default as TreeSelect } from './TreeSelect';
-export { default as DatePicker } from './DatePicker';
-export { default as TimePicker } from './TimePicker';
