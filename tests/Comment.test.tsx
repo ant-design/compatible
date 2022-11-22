0a327d023aa7dd8de271f8bf46f62ef9d17a9253
二货机器人 <smith3816@gmail.com>
2022-11-23 00:18:21 +0800

    test: update snapshot

--- tests/Comment.test.tsx
+++ tests/Comment.test.tsx
@@ -4,11 +4,6 @@ import { Comment } from '../src';
 import { render } from '@testing-library/react';
 
 describe('Comment', () => {
-  it('work', () => {
-    const { container } = render(<Comment content="" />);
-    expect(container).toMatchSnapshot();
-  });
-
   it('no hash', () => {
     const { container } = render(
       <ConfigProvider
--- tests/Form.test.tsx
+++ tests/Form.test.tsx
@@ -4,11 +4,6 @@ import { Form } from '../src';
 import { render } from '@testing-library/react';
 
 describe('Form', () => {
-  it('work', () => {
-    const { container } = render(<Form />);
-    expect(container).toMatchSnapshot();
-  });
-
   it('no hash', () => {
     const { container } = render(
       <ConfigProvider
